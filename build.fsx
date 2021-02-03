c30b7724c64a92f9839b9abea9a3f4a1f7c69e20
Jlobblet <jlobblet-github@gmx.co.uk>
2021-02-03 04:50:44 +0000

    Continuous integration v1

---
+++ build.fsx
@@ -0,0 +1,143 @@
+#r "paket:
+nuget Fantomas.Extras
+nuget Fake.DotNet.Cli
+nuget Fake.DotNet.NuGet
+nuget Fake.IO.FileSystem
+nuget Fake.Core.Target //"
+#load ".fake/build.fsx/intellisense.fsx"
+
+open System
+open Fantomas
+open Fantomas.Extras
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+Target.initEnvironment ()
+
+[<Literal>]
+let NugetDownload = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+
+let (|NullOrWhitespaceString|ValueString|) (str: string) =
+    if String.isNullOrWhiteSpace (str) then
+        NullOrWhitespaceString
+    else
+        ValueString str
+
+let tryGetEnvironmentVariable name =
+    let tryString =
+        function
+        | NullOrWhitespaceString -> None
+        | ValueString var -> Some var
+
+    [ Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process)
+      Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.User)
+      Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Machine) ]
+    |> List.choose tryString
+    |> List.tryHead
+
+let getEnvironmentVariable name =
+    match tryGetEnvironmentVariable name with
+    | None -> failwith $"Could not find environment variable {name}"
+    | Some var -> var
+
+let solution = "Fugit.sln"
+
+// Linting
+
+Target.create "Check F# formatting"
+<| fun _ ->
+    let result =
+        !! "**/*.fs"
+        -- "**/bin/**/*.fs"
+        -- "**/obj/**/*.fs"
+        |> FakeHelpers.checkCode
+        |> Async.RunSynchronously
+
+    if result.IsValid then
+        Trace.log "No files need formatting"
+    elif result.NeedsFormatting then
+        Trace.log "The following files need formatting:"
+        result.Formatted |> List.iter Trace.log
+        failwith "Some files need formatting, check output for more info"
+    else
+        Trace.log $"Errors while formatting: {result.Errors}"
+
+// Cleaning
+
+Target.create "Clean"
+<| fun _ ->
+    !! "src/**/bin"
+    ++ "src/**/obj"
+    ++ "tests/**/bin"
+    ++ "tests/**/obj"
+    ++ "NuGet"
+    |> Shell.cleanDirs
+
+// Restore
+
+Target.create "Restore"
+<| fun _ -> solution |> DotNet.restore id
+
+// Build
+
+Target.create "Build"
+<| fun _ ->
+    !! "src/**/*.*proj"
+    |> Seq.iter (DotNet.build (fun p ->
+        { p with
+            Configuration = DotNet.BuildConfiguration.Release
+            NoLogo = true
+            NoRestore = true}))
+
+Target.create "Build tests"
+<| fun _ ->
+    !! "tests/**/*.*proj"
+    |> Seq.iter (DotNet.build (fun p ->
+        { p with
+            NoLogo = true
+            NoRestore = true}))
+
+// Running tests
+
+Target.create "Run tests"
+<| fun _ ->
+    !! "tests/**/*.*proj"
+    |> Seq.iter (DotNet.test (fun p ->
+        { p with
+            NoLogo = true
+            NoRestore = true}))
+
+// NuGet
+
+Target.create "Create NuGet packages"
+<| fun _ ->
+    !! "src/**/*.*proj"
+    |> Seq.iter (DotNet.pack (fun p ->
+        { p with
+            NoLogo = true
+            OutputPath = Some "NuGet"
+             }))
+
+// Combinations
+
+Target.create "All" ignore
+Target.create "Deploy" ignore
+
+// Dependencies
+
+"Clean" ==> "Restore" ==> "Build"
+
+"Build" ==> "Run tests"
+
+"Check F# formatting" ?=> "Build" ?=> "Run tests"
+
+"All" <== [ "Check F# formatting"; "Run tests" ]
+
+"All" ==> "Create NuGet packages"
+
+// Run
+
+Target.runOrDefaultWithArguments "All"
